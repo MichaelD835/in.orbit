d21eec149026cb68f50365eb33b6b3d90f7c4a54
Michael Douglas <michaeldouglas835@gmail.com>
2024-09-15 21:31:23 -0300

    configuration completed

---
+++ src/db/schema.ts
@@ -0,0 +1,21 @@
+import { pgTable, text, integer, timestamp } from 'drizzle-orm/pg-core'
+import { createId } from '@paralleldrive/cuid2'
+
+export const goals = pgTable('goals', {
+  id: text('id').primaryKey().$defaultFn(() => createId),
+  title: text('title').notNull(),
+  desireWeeklyFrequency: integer('desired_weekly_frequency').notNull(),
+  createAt: timestamp('created_at', { withTimezone: true })
+    .notNull()
+    .defaultNow(),
+})
+
+export const goalCompletions = pgTable('goal_completions', {
+  id: text('id').primaryKey().$defaultFn(() => createId),
+  goalID: text('goal_id')
+    .references(() => goals.id)
+    .notNull(),
+  createAt: timestamp('created_at', { withTimezone: true })
+    .notNull()
+    .defaultNow(),
+})
